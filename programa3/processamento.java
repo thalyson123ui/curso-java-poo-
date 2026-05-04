d3f50b192ae78e1ff18d117e90e3ade4a5d02d8d
THALYSON JOSE DA ROSA SILVA <thalysonjosedarosasilva@gmail.com>
2026-05-04 13:20:53 -0300

    atualização

---
+++ programa3/processamento.java
@@ -0,0 +1,16 @@
+package programa3;
+
+public class processamento {
+    
+    public static void main(String[] args) {
+        
+        double a;
+        int b;
+
+        a = 5.0;
+        b = (int) a;
+
+        System.out.println("Valor de a: " + a);
+        System.out.println("Valor de b: " + b);
+    }
+}
