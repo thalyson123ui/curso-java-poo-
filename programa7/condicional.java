a901406bf51a4619987fcb313a4aba9d2daaa392
THALYSON JOSE DA ROSA SILVA <thalysonjosedarosasilva@gmail.com>
2026-05-06 15:43:39 -0300

    atualização

---
+++ programa7/condicional.java
@@ -0,0 +1,23 @@
+package programa7;
+import java.util.Scanner;
+public class condicional {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int hora;
+
+        System.out.println("quantas horas?");
+        hora = sc.nextInt();
+
+        if (hora < 12) {
+            System.out.println("bom dia");
+        } else {
+            if (hora < 18) {
+                System.out.println("boa tarde");
+            } else {
+                System.out.println("boa noite");
+            }
+        }
+
+        sc.close();
+    }
+}
