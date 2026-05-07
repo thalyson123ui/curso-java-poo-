f8527aacf16e9769d4592feaf40d5523d0ba83e3
THALYSON JOSE DA ROSA SILVA <thalysonjosedarosasilva@gmail.com>
2026-05-07 16:16:19 -0300

    atualização

---
+++ programa8/operadoresdeatribuicaocumulativo.java
@@ -0,0 +1,20 @@
+package programa8;
+
+import java.util.Locale;
+import java.util.Scanner;
+public class operadoresdeatribuicaocumulativo {
+    public static void main(String[] args) {
+       Locale.setDefault(Locale.US);
+       Scanner sc = new Scanner(System.in);
+
+       int minutos = sc.nextInt();
+
+       double conta = 50.00;
+         if (minutos > 100) {
+              conta += (minutos - 100) * 2.00;
+         }
+         System.out.printf("Valor da conta: R$ %.2f%n", conta);
+
+         sc.close();
+    }
+}
