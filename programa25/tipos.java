c6d6f848ab9df609e2494f50b75275f7813aacef
THALYSON JOSE DA ROSA SILVA <thalysonjosedarosasilva@gmail.com>
2026-05-25 11:05:17 -0300

    atualização

---
+++ programa25/tipos.java
@@ -0,0 +1,9 @@
+package programa25;
+
+public class tipos {
+    public static void main(String[] args) {
+        int p;
+        p = 10;
+        System.out.println("El valor de p es: " + p);
+    }
+}
