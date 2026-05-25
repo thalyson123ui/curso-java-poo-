5b67224e4585b35240a44a7d8a0011ca897d724d
THALYSON JOSE DA ROSA SILVA <thalysonjosedarosasilva@gmail.com>
2026-05-25 11:47:49 -0300

    atualização

---
+++ programa26/vetores.java
@@ -0,0 +1,26 @@
+package programa26;
+import java.util.Scanner;
+import java.util.Locale;
+public class vetores {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        double[] vect = new double[n];
+
+        for (int i = 0; i < n; i++) {
+            vect[i] = sc.nextDouble();
+        }
+
+        double sum = 0.0;
+        for (int i = 0; i < n; i++) {
+            sum += vect[i];
+        }
+        double avg = sum / n;
+
+        System.out.printf("Average: %.2f%n", avg);
+
+        sc.close();
+    }
+}
